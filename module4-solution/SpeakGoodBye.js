84a8a38210cd49a70cf3c86686472197f4b4feb1
Donghyeon Won <won.donghyeon@gmail.com>
2021-03-29 07:33:23 +0900

    indent

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -16,10 +16,11 @@
     // byeSpeaker object instead of being a standalone function.
     // See Lecture 52, part 2
     byeSpeaker.speak = function(name) {
-            console.log(speakWord + " " + name);
-        }
-        // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
-        // 'byeSpeaker' on the global scope as well.
-        // xxxx.xxxx = byeSpeaker;
+        console.log(speakWord + " " + name);
+    }
+
+    // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+    // 'byeSpeaker' on the global scope as well.
+    // xxxx.xxxx = byeSpeaker;
     window.byeSpeaker = byeSpeaker;
 })(window);
